f95cf980961c5734c2d8cdcfd4794b59eeeeb99d
Nikolai Stoilov <nikolai.stoilov@certivox.com>
2016-02-04 11:00:09 +0200

    added test svg

---
+++ testsvg/main.js
@@ -0,0 +1,7 @@
+
+function shrug() {
+    document.getElementById("button1").setAttribute("value", " つ -_- ༽つ");
+    setTimeout(function(){ document.getElementById("button1").setAttribute("value", " つ ◕_◕ ༽つ"); }, 100);    
+ }
+  
+
