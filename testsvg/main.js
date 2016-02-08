99775d9014e6c14420d89d4e434fa646605c50c6
Nikolai Stoilov <nikolai.stoilov@certivox.com>
2016-02-08 17:01:08 +0200

    added js to svg test

--- testsvg/main.js
+++ testsvg/main.js
@@ -1,7 +1,23 @@
 
 function shrug() {
     document.getElementById("button1").setAttribute("value", " つ -_- ༽つ");
-    setTimeout(function(){ document.getElementById("button1").setAttribute("value", " つ ◕_◕ ༽つ"); }, 100);    
+    
+    document.getElementById("text").setAttribute("transform", "rotate(-20 20,80)")
+    document.getElementById("text").setAttribute("fill", "red");
+    for (var i = 0; i < document.getElementsByClassName("inner").length; i++) {
+	document.getElementsByClassName("inner")[i].setAttribute("fill", "yellow");
+}
+ 
+    setTimeout(function(){
+    document.getElementById("button1").setAttribute("value", " つ ◕_◕ ༽つ");    
+     }, 100);
+    setTimeout(function(){        
+    document.getElementById("text").setAttribute("transform", "rotate(-25 20,80)");
+    document.getElementById("text").setAttribute("fill", "black");
+    for (var i = 0; i < document.getElementsByClassName("inner").length; i++) {
+	document.getElementsByClassName("inner")[i].setAttribute("fill", "white");
+
+}
+     }, 300);
  }
   
-
